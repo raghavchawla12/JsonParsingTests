fff66ca27d945d622b95cc8a58e8dffce61dbaf6
Raghav Chawla <raghavchawlara@gmail.com>
2022-08-18 22:44:26 +0530

    Test Cases Done

--- tests/tests_problem_to_solve.py
+++ tests/tests_problem_to_solve.py
@@ -1,14 +1,41 @@
 import pytest
 
-from test_data import given_json
+from test_data.given_json import GivenJson
+from utilities.utils_class import utilsClass
 
 
-class TestProblems:
+class TestProblems(utilsClass):
 
-    def test_01_validate_that_team_has_only_four_foreign_players(self, get_test_cases_data):
+    @pytest.mark.parametrize("count_exp", [4])
+    def test_01_validate_that_team_has_only_four_foreign_players(self, count_exp):
         log = self.getLogger()
-        json = get_test_cases_data['json']
+        players_list = GivenJson.needed_json
+        log.debug("Got Json through Test Data Package using PyTest Fixtures in Test 01")
+        count = 0
+        try:
+            for i in range(0, len(players_list)):
+                countries = players_list[i]['country']
+                if countries != "India":
+                    count = count + 1
+            assert count == count_exp
+            log.info("Test Case Passed with correct Foreign Player Count i.e. 4")
+        except Exception as e:
+            log.error("Test Case Failed")
+            raise e
 
-    @pytest.fixture(params=given_json.GivenJson)
-    def get_test_cases_data(self, request):
-        return request.param
+    def test_02_validate_that_team_has_atleast_one_wicket_keeper(self):
+        log = self.getLogger()
+        players_list = GivenJson.needed_json
+        log.debug("Got Json through Test Data Package using PyTest Fixtures in Test 02")
+        count = 0
+        try:
+            for i in range(0, len(players_list)):
+                role = players_list[i]['role']
+                if role == "Wicket-keeper":
+                    count = count + 1
+            assert count >= 1
+            log.info("Test Case Passed, There is atleast one wicket keeper present. "
+                     "Total Wicketkeepers are " + str(count))
+        except Exception as e:
+            log.error("Test Case Failed")
+            raise e
